65692fcdba17bed49c34d44743a9f8e858bbce5e
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-08-06 21:00:00 +0900

    update tailwind config

--- library/tailwind.config.js
+++ library/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  important: "react-portable-preview",
   theme: {
     extend: {},
   },
